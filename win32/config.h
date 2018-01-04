7667c7c6a5c444888a6a36a57c21cab18a8caa37
Ricardo Constantino <wiiaboo@gmail.com>
2018-01-04 00:00:41 +0000

    VS2015: add flac support
    Signed-off-by: Mark Harris <mark.hsj@gmail.com>

--- win32/config.h
+++ win32/config.h
@@ -1,6 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/* comment to disable flac dependency */
+#define HAVE_LIBFLAC          1
+/* comment to compile with dynamic flac */
+#define FLAC__NO_DLL
+
 #define OUTSIDE_SPEEX         1
 #define OPUSTOOLS             1
 
@@ -9,6 +14,15 @@
 #define getpid _getpid
 #define USE_ALLOCA            1
 #define FLOATING_POINT        1
+
+#ifdef HAVE_LIBFLAC
+#ifdef FLAC__NO_DLL
+    #pragma comment(lib, "libFLAC_static.lib")
+#else
+    #pragma comment(lib, "libFLAC_dynamic.lib")
+#endif
+#endif
+
 #define SPX_RESAMPLE_EXPORT
 #define __SSE__
 
